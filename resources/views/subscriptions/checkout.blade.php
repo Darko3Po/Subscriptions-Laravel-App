04a26f53adc8e8809c38047e84a4834629da93e9
Darko3Po <potajacdarko9@gmail.com>
2024-02-06 17:52:13 +0100

    Added form on checkout page

--- resources/views/subscriptions/checkout.blade.php
+++ resources/views/subscriptions/checkout.blade.php
@@ -6,17 +6,23 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-2x mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                         <form action="POST">
                             <div>
                                 <x-input-label for="name" :value="__('Name of card')" />
-                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="username" />
+                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" />
                                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
-                            <div>
+                            <div class="mt-4">
                                 <x-input-label for="card" :value="__('Card details')" />
+
+                            </div>
+                            <div class="flex items-center justify-end mt-4">
+                                <x-primary-button class="ms-3">
+                                    Pay
+                                </x-primary-button>
                             </div>
                         </form>
                 </div>
