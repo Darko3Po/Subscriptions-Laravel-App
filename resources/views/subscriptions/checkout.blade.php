c264b87d0c71e174fce45b679336e805b73b1a32
Darko3Po <potajacdarko9@gmail.com>
2024-02-07 14:40:44 +0100

    Added Stripe credit card system

--- resources/views/subscriptions/checkout.blade.php
+++ resources/views/subscriptions/checkout.blade.php
@@ -6,10 +6,10 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-2x mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto w-4/5">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                        <form action="POST">
+                        <form action="" class="w-4/5">
                             <div>
                                 <x-input-label for="name" :value="__('Name of card')" />
                                 <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" />
@@ -17,7 +17,7 @@
                             </div>
                             <div class="mt-4">
                                 <x-input-label for="card" :value="__('Card details')" />
-
+                                <div id="card-element"></div>
                             </div>
                             <div class="flex items-center justify-end mt-4">
                                 <x-primary-button class="ms-3">
@@ -29,4 +29,18 @@
             </div>
         </div>
     </div>
+{{--    {{ config('cashier.key')  }}    --}}
+{{--    STRIPE_KEY=pk_test_51OfHGtDbT4THc2zuZG7YOK6slxOuSKrSKQFMT4B5SjPoljmGoYmoOg1NXO6e91sBNdKBHMHCvPP7dLp25dKmiPV200NqngSJUr--}}
+    <script>
+        const strip = Stripe('{{ config('cashier.key') }}')
+
+        const elements = strip.elements();
+
+        const cardElement = elements.create('card');
+
+        cardElement.mount('#card-element');
+
+        console.log(strip);
+    </script>
+
 </x-app-layout>
