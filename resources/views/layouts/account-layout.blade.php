d65d56e054748df25b42e1ce7463bc3c743ffcc4
Darko3Po <potajacdarko9@gmail.com>
2024-02-16 18:51:33 +0100

    Update Account Layout blade

--- resources/views/layouts/account-layout.blade.php
+++ resources/views/layouts/account-layout.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-        <div class=" flex py-12 max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 md:space-y-0 md:grid grid-cols-8 gap-6">
+        <div class="flex py-12 max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 md:space-y-0 md:grid grid-cols-8 gap-6">
             <div class=" w-1/2 py-12">
                 <ul class="mb-10">
                     <li><a href="{{ route('account')  }}" class="hover:text-blue-500">Account</a></li>
